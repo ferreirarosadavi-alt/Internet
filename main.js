9f6891ff9512aa3502d1c22319603cec7737e029
ferreirarosadavi-alt <ferreira.rosa.davi@escola.pr.gov.br>
2026-07-08 18:42:54 -0300

    Create main.js

---
+++ main.js
@@ -0,0 +1,7 @@
+// Seleciona o botão pelo ID
+const botaoRefletir = document.getElementById('btn-Refletir');
+
+// Adiciona o evento de clique
+botaoRefletir.addEventListener('click', function() {
+    alert('Curiosidade: Sem a internet, você não estaria lendo esta página agora! Setores como bancos, hospitais e transportes voltariam a operar 100% no papel.');
+});
